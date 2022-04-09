a35b979a51e8032be7c229405f2d61827382c10f
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-04-09 07:07:01 +0200

    sdf2

--- components/body/Home/components/Reel/Reels.js
+++ components/body/Home/components/Reel/Reels.js
@@ -4,23 +4,28 @@ import OneReel from "./OneReel";
 import { UsersDemo } from "./../UserDemo.js";
 
 const Reels = () => {
-  const [Users, setUsers] = useState([{username: "Heeeeee" }]);
+  const [Users, setUsers] = useState([{ username: "Heeeeee" }]);
   const Url = "https://fakestoreapi.com/users";
-   
+
   useEffect(() => {
     const fechData = async () => {
       const res = await fetch(Url);
       const data = await res.json();
-      setUsers(UsersDemo.slice(0 , 6));
+      setUsers(UsersDemo.slice(0, 6));
       console.log(Users);
     };
     fechData();
-  }, []);
+  }, [Users]);
   return (
     <div className={Style.container}>
       <div className={Style.reels_container}>
         {Users.map(({ image, username }, index) => (
-          <OneReel Image={image} UserName={username} Index={index} />
+          <OneReel
+            Image={image}
+            UserName={username}
+            Index={index}
+            key={index}
+          />
         ))}
       </div>
     </div>
