98e0438d06632c4b996926a84b593dbdc8440116
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-04-22 21:52:54 +0200

    sd

--- components/layout/UserFech.js
+++ components/layout/UserFech.js
@@ -1,28 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useState , useEffect } from "react";
 import { io } from "socket.io-client";
 import Cookies from "js-cookie";
 const UserFech = () => {
   let socket;
   const sendCookie = Cookies.get("user_ref");
+  const [Host , setHost] = useState("")
   useEffect(() => {
+      if (window.location.hostname ==="localhost"){
+          setHost("http://localhost:3000");
+      }else{
+          setHost(`http${window.location.hostname}`);
+      }
     const fetchData = async () => {
       const basurl = process.env.NEXT_PUBLIC_ANALYTICS_ID;
-      console.log("url :" + basurl);
+      console.log("host :" + Host);
+      console.log( "url :" + basurl);;
       socket = io();
       socket.connect();
-      await fetch(
-        `https://instagraam-nine.vercel.app/api/user/auth/login/sending`
-      ).then(() => {
-        if (sendCookie) {
-          socket.emit("send-cookie", sendCookie);
-          socket.on("output-user", (payload) => {
-            console.log("user is " + payload);
-          });
-        } else {
-          socket.emit("send-cookie", sendCookie);
-          socket.on("user-res", (payload) => {});
-        }
-      });
+      await fetch(`${Host}/api/user/auth/login/sending`)
+      if (sendCookie) {
+        socket.emit("send-cookie", sendCookie);
+        socket.on("output-user", (payload) => {
+          console.log("user is " + payload);
+        });
+      } else {
+        socket.emit("send-cookie", sendCookie);
+        socket.on("user-res", (payload) => {});
+      }
     };
     fetchData();
     return () => {
