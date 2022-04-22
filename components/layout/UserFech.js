e36bb57aebc521b09446e145d865d8946a8a5b01
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-04-22 21:34:58 +0200

    sdsdsoid

--- components/layout/UserFech.js
+++ components/layout/UserFech.js
@@ -7,19 +7,22 @@ const UserFech = () => {
   useEffect(() => {
     const fetchData = async () => {
       const basurl = process.env.NEXT_PUBLIC_ANALYTICS_ID;
-      console.log( "url :" + basurl);;
+      console.log("url :" + basurl);
       socket = io();
       socket.connect();
-      await fetch(`${basurl}/api/user/auth/login/sending`);
-      if (sendCookie) {
-        socket.emit("send-cookie", sendCookie);
-        socket.on("output-user", (payload) => {
-          console.log("user is " + payload);
-        });
-      } else {
-        socket.emit("send-cookie", sendCookie);
-        socket.on("user-res", (payload) => {});
-      }
+      await fetch(
+        `https://instagraam-nine.vercel.app/api/user/auth/login/sending`
+      ).then(() => {
+        if (sendCookie) {
+          socket.emit("send-cookie", sendCookie);
+          socket.on("output-user", (payload) => {
+            console.log("user is " + payload);
+          });
+        } else {
+          socket.emit("send-cookie", sendCookie);
+          socket.on("user-res", (payload) => {});
+        }
+      });
     };
     fetchData();
     return () => {
